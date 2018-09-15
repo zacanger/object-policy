139a01edf61f08598a92ef647a3cf824a0481d3d
zacanger <zac@zacanger.com>
2018-09-14 19:47:40 -0600

    Make it do the thing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = {}
+const fixCase = require('zeelib/lib/camel-case-to-lisp-case')
+
+module.exports = (opts = {}) => {
+  const directives = Object.keys(opts)
+    .reduce((p, c) => {
+      const fixed = fixCase(c)
+      p[fixed] = opts[c].join(' ')
+      return p
+    }, {})
+
+  return Object.keys(directives)
+    .reduce((p, c) => {
+      p += `${c}: ${directives[c]}; `
+      return p
+    }, '').trim()
+}
---
+++ index.test.js
@@ -0,0 +1,13 @@
+const tape = require('tape')
+const policy = require('./')
+
+tape.test('objectPolicy', (t) => {
+  t.equal(
+    policy({
+      fooBar: [ 'a', 'b' ],
+      'baz-quux': [ 'one', 'two' ]
+    }),
+    'foo-bar: a b; baz-quux: one two;'
+  )
+  t.end()
+})
